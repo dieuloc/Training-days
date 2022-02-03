789008dac45b66e1a8f9c7b5848e65a64a66d7c2
Loc Dieu <tuk36760@temple.edu>
2022-02-03 17:30:08 -0500

    day 6 works
    update with day 6 homework

---
+++ 02UnderstandingTypes/02UnderstandingTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine($"        MIN   MAX");
+Console.WriteLine($"sbyte:\t  {sbyte.MinValue}  {sbyte.MaxValue}");
+Console.WriteLine($"byte:\t  {byte.MinValue}  {byte.MaxValue}");
+Console.WriteLine($"short:\t  {short.MinValue}  {short.MaxValue}");
+Console.WriteLine($"ushort:\t  {ushort.MinValue}  {ushort.MaxValue}");
+Console.WriteLine($"int:\t  {int.MinValue}  {int.MaxValue}");
+Console.WriteLine($"uint:\t  {uint.MinValue}  {uint.MaxValue}");
+Console.WriteLine($"long:\t  {long.MinValue}  {long.MaxValue}");
+Console.WriteLine($"ulong:\t  {ulong.MinValue}  {ulong.MaxValue}");
+Console.WriteLine($"float:\t  {float.MinValue}  {float.MaxValue}");
+Console.WriteLine($"double:\t  {double.MinValue}  {double.MaxValue}");
+Console.WriteLine($"decimal:\t  {decimal.MinValue}  {decimal.MaxValue}");
+
+
---
+++ TimeConversion/TimeConversion/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("please input the number of century = ");
+int cent = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{cent} centuries = {cent * 100} years = {cent * 36524.2} days = {cent * 876581.3} hours = {cent * 52594876.7} minute = {cent * 3155692600} seconds = {cent * 3155692600000.1} miliseconds");
